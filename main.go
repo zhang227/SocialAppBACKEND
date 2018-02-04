0e781b266e4eed91ddeb7f18c71d0799474722a1
zhang227 <zhang.jinf@husky.neu.edu>
2018-02-04 17:35:55 -0600

    add elastic search and change the fake posr to real

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 import (
+	elastic "gopkg.in/olivere/elastic.v3"
 	"fmt"
 	"net/http"
 	"encoding/json"
 	"log"
 	"strconv"
+	"reflect"
 )
+
 
 type Location  struct{
 	Lat float64 `json:"lat"`
@@ -44,7 +47,14 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 //handle search
 const (
+	INDEX = "around"
+	TYPE = "post"
 	DISTANCE = "200km"
+	// Needs to update
+	//PROJECT_ID = "around-xxx"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://YOUR_ES_IP_ADDRESS:9200"
 )
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -58,29 +68,57 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		ran = val + "km"
 	}
 	fmt.Fprintf(w, "Search Received: %f^f%s", lat,lon,ran)
-
-	//return a fake post
-	p := &Post{
-		User:"1111",
-		Message:"一生必去的100个地方",
-		Location: Location{
-			Lat:lat,
-			Lon:lon,
-
-		},
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
 	}
-	//wrap to json
-	js, err := json.Marshal(p)
+
+	// Define geo distance query as specified in
+	// https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	// Some delay may range from seconds to minutes. So if you don't get enough results. Try it later.
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
+		p := item.(Post) // p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
 	if err != nil {
 		panic(err)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
-
-
-
 }
+
 
 
